39e4d661e1f080f2cb652e7e47b3a164d03584cc
David Cairns <dcairns@butterflynetinc.com>
2022-06-08 09:26:06 -0700

    Initial Commit

---
+++ Shared/BeckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeckersApp.swift
+//  Shared
+//
+//  Created by David Cairns on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BeckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
